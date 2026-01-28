ab7cbdfe251004efca081d8af3479b811986d5de
Namrata Kapoor <namrata.skapoor@gmail.com>
2026-01-28 00:41:07 -0800

    Convert to app.config.js to fix google services error

---
+++ app.config.js
@@ -0,0 +1,60 @@
+// app.config.js
+module.exports = {
+  expo: {
+    name: "my-mental-health-app",
+    slug: "my-mental-health-app",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    newArchEnabled: false,
+    splash: {
+      image: "./assets/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    ios: {
+      bundleIdentifier: "com.namrata.skapoor.mymentalhealthapp",
+      supportsTablet: true,
+      infoPlist: {
+        ITSAppUsesNonExemptEncryption: false
+      }
+    },
+    android: {
+      softwareKeyboardLayoutMode: "pan",
+      permissions: ["INTERNET"],
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      },
+      package: "com.namrata.skapoor.mymentalhealthapp",
+      googleServicesFile: process.env.GOOGLE_SERVICES_JSON || "./google-services.json",
+    },
+    web: {
+      favicon: "./assets/favicon.png"
+    },
+    experiments: {
+      tsconfigPaths: true
+    },
+    packagerOpts: {
+      config: "metro.config.js"
+    },
+    plugins: [
+      [
+        "expo-build-properties",
+        {
+          android: {
+            usesCleartextTraffic: true,
+            networkInspector: true
+          }
+        }
+      ]
+    ],
+    extra: {
+      eas: {
+        projectId: "5d8f0aa6-a9ee-40a7-b25f-11a0ee91904b"
+      }
+    },
+    owner: "namrata.skapoor"
+  }
+};
